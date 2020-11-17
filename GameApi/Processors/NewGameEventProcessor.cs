22febcaa5d7ee13ca9044a9dd0fb787f3a36fe51
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-11-16 21:48:21 -0500

    Game Service New Game Listener
    Added some notes on how to hand new game events.

--- GameApi/Processors/NewGameEventProcessor.cs
+++ GameApi/Processors/NewGameEventProcessor.cs
@@ -26,6 +26,16 @@ namespace GameApi.Processors
         }
 
         private Task HandleEvent(Event evnt) {
+            //generate related aggregate from it's stream
+            //this db loading processor so check if this event
+            //id is in the data base
+            //otherwise would check the aggregate list of event ids
+            //if the event id is already there we processed it and can
+            //skip this event
+            //At this point we can process the event
+            //for this event since it's a new object we can see if the id
+            //is already there if so we can skip this event
+            //otherwise we write the game to the database
             Console.WriteLine("Received Event: " + evnt);
             return Task.CompletedTask;
         }
