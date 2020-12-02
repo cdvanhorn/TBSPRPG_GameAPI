1db5d9e916b3305409176cdedc05369c48ad836e
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-01 21:39:06 -0500

    New Game Listener
    Keep track of a service id and the place in each event stream.

---
+++ GameApi/Entities/EventIndex.cs
@@ -0,0 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GameApi.Entities {
+    public class EventIndex {
+        [BsonElement("event_name")]
+        public string EventName { get; set; }
+
+        [BsonElement("index")]
+        public string Index { get; set;}
+    }
+}
---
+++ GameApi/Repositories/ServiceRepository.cs
@@ -0,0 +1,42 @@
+using GameApi.Entities;
+
+using MongoDB.Driver;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using TbspRgpLib.Settings;
+
+namespace GameApi.Repositories {
+    public interface IServiceRespository {
+        Task<List<Service>> GetAllServices();
+
+        Task<Service> GetServiceByName(string name);
+    }
+
+    public class ServiceRepository : IServiceRespository {
+        private readonly IDatabaseSettings _dbSettings;
+
+        private readonly IMongoCollection<Service> _services;
+
+        public ServiceRepository(IDatabaseSettings databaseSettings) {
+            _dbSettings = databaseSettings;
+
+            var connectionString = $"mongodb+srv://{_dbSettings.Username}:{_dbSettings.Password}@{_dbSettings.Url}/{_dbSettings.Name}?retryWrites=true&w=majority";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(_dbSettings.Name);
+
+            _services = database.GetCollection<Service>("services");
+        }
+
+        public Task<List<Service>> GetAllServices() {
+            return _services.Find(service => true).ToListAsync();
+        }
+
+        public Task<Service> GetServiceByName(string name) {
+            return _services.Find(service => 
+                name.ToLower() == service.Name.ToLower()).FirstOrDefaultAsync();
+        }
+    }
+}
