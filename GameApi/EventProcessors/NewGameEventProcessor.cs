63d3c5d22bfd5bdfa52c1bac5b2d3d086c4deeb0
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-01-16 22:02:08 -0500

    MapAPI
    Cleaned up hosted service construction by using a scope factory.

--- GameApi/EventProcessors/NewGameEventProcessor.cs
+++ GameApi/EventProcessors/NewGameEventProcessor.cs
@@ -12,6 +12,7 @@ using TbspRpgLib.Events;
 using TbspRpgLib.Entities;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GameApi.EventProcessors
 {
@@ -20,47 +21,19 @@ namespace GameApi.EventProcessors
         private IGameAggregateAdapter _gameAdapter;
         private IGameLogic _gameLogic;
         private GameContext _context;
+        private readonly IServiceScope _scope;
 
-        public MyNewGameEventProcessor(IEventStoreSettings eventStoreSettings) :
+        public MyNewGameEventProcessor(IEventStoreSettings eventStoreSettings, IServiceScopeFactory scopeFactory) :
             base("game", eventStoreSettings){
             _gameAdapter = new GameAggregateAdapter();
-
-            //need to create a db context
-            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-            var optionsBuilder = new DbContextOptionsBuilder<GameContext>();
-            optionsBuilder.UseNpgsql(connectionString);
-            _context = new GameContext(optionsBuilder.Options);
-
-            //create the game service
-            GameRepository gameRepository = new GameRepository(_context);
-            EventService eventService = new EventService(eventStoreSettings);
-            GameService gameService = new GameService(gameRepository);
-
-            //create the adventure service
-            AdventureRepository adventureRepository = new AdventureRepository(_context);
-            AdventureService adventureService = new AdventureService(adventureRepository);
-
-            _gameLogic = new GameLogic(
-                new EventAdapter(),
-                eventService,
-                gameService,
-                adventureService
-            );
-            
-            //initialize the services
+            _scope = scopeFactory.CreateScope();
+            _context = _scope.ServiceProvider.GetRequiredService<GameContext>();
+            _gameLogic = _scope.ServiceProvider.GetRequiredService<IGameLogic>();
             this.InitializeServices(_context);
         }
 
         protected override async void HandleEvent(Aggregate aggregate, string eventId, ulong position) {
-            //generate related aggregate from it's stream
-            //this db loading processor so check if this event
-            //id is in the data base
-            //otherwise would check the aggregate list of event ids
-            //if the event id is already there we processed it and can
-            //skip this event
             GameAggregate gameAggregate = (GameAggregate)aggregate;
-            //we need to convert the aggregate to a game object and insert in to the database
-            //but we don't want to insert duplicates
             Game game = _gameAdapter.ToEntity(gameAggregate);
             
             //if the game is missing fields ignore it
