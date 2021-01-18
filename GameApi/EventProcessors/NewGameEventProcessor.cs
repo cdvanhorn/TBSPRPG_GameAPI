28294c94d666577b6348ad1cf9a73e89cb6bcab4
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-01-17 23:37:08 -0500

    MapAPI
    Something broke.

--- GameApi/EventProcessors/NewGameEventProcessor.cs
+++ GameApi/EventProcessors/NewGameEventProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 
 using GameApi.Adapters;
 using GameApi.Entities;
@@ -18,42 +19,46 @@ namespace GameApi.EventProcessors
 {
     public class MyNewGameEventProcessor : NewGameEventProcessor
     {
-        private IGameAggregateAdapter _gameAdapter;
-        private IGameLogic _gameLogic;
-        private GameContext _context;
-        private readonly IServiceScope _scope;
+        private readonly IServiceScopeFactory _scopeFactory;
 
         public MyNewGameEventProcessor(IEventStoreSettings eventStoreSettings, IServiceScopeFactory scopeFactory) :
             base("game", eventStoreSettings){
-            _gameAdapter = new GameAggregateAdapter();
-            _scope = scopeFactory.CreateScope();
-            _context = _scope.ServiceProvider.GetRequiredService<GameContext>();
-            _gameLogic = _scope.ServiceProvider.GetRequiredService<IGameLogic>();
-            this.InitializeServices(_context);
+            _scopeFactory = scopeFactory;
+            var scope = _scopeFactory.CreateScope();
+            this.InitializeServices(
+                scope.ServiceProvider.GetRequiredService<GameContext>()
+            );
         }
 
         protected override async void HandleEvent(Aggregate aggregate, string eventId, ulong position) {
             GameAggregate gameAggregate = (GameAggregate)aggregate;
+            GameAggregateAdapter _gameAdapter = new GameAggregateAdapter();
             Game game = _gameAdapter.ToEntity(gameAggregate);
-            
-            //if the game is missing fields ignore it
-            if(game.UserId == null || game.AdventureId == null)
-                return;
-            Console.WriteLine($"Writing Game {game.Id} {position}!!");
 
-            //make sure the event id is a valid guid
-            Guid eventguid;
-            if(!Guid.TryParse(eventId, out eventguid))
-                return;
+            using(var scope = _scopeFactory.CreateScope()) {
+                var context = scope.ServiceProvider.GetRequiredService<GameContext>();
+                var gameLogic = scope.ServiceProvider.GetRequiredService<IGameLogic>();
+                InitializeServices(context);
 
-            //update the game
-            await _gameLogic.AddGame(game);
-            //update the event type position
-            await UpdatePosition(position);
-            //update the processed events
-            await AddEventTracked(eventId);
-            //save the changes
-            _context.SaveChanges();
+                //if the game is missing fields ignore it
+                if(game.UserId == null || game.AdventureId == null)
+                    return;
+                Console.WriteLine($"Writing Game {game.Id} {position} {eventId}!!");
+
+                //make sure the event id is a valid guid
+                Guid eventguid;
+                if(!Guid.TryParse(eventId, out eventguid))
+                    return;
+
+                //update the game
+                await gameLogic.AddGame(game);
+                //update the event type position
+                await UpdatePosition(position);
+                //update the processed events
+                await AddEventTracked(eventId);
+                //save the changes
+                context.SaveChanges();
+            }
         }
     }
 }
